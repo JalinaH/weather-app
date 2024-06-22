e30222b1e1b3ce8a6c0c62cd9fcc2335cdf44b50
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-22 19:01:56 +0530

    chore: Remove react-helmet from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import Weather from "./components/Weather";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="app">
-      <Helmet>
-        <title>Weather App</title>
-        <meta
-          name="description"
-          content="Check current weather conditions in your city with Weather App."
-        />
-      </Helmet>
       <Weather />
     </div>
   );
